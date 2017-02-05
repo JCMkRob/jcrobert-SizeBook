d15243ef8ff7baecf97de97650f01be2a0b61300
JCMkRob <jcrobert@ualberta.ca>
2017-02-05 00:52:14 -0700

    Added saving/loading, and viewing of existing records.

---
+++ app/src/main/java/com/example/joshuarobertson/sizebook/Entry.java
@@ -0,0 +1,8 @@
+package com.example.joshuarobertson.sizebook;
+
+/**
+ * Created by joshuarobertson on 2017-02-04.
+ */
+
+public class Entry {
+}
---
+++ app/src/main/java/com/example/joshuarobertson/sizebook/ViewEntry.java
@@ -0,0 +1,7 @@
+package com.example.joshuarobertson.sizebook;
+
+/**
+ * Created by joshuarobertson on 2017-02-04.
+ */
+public class ViewEntry {
+}
